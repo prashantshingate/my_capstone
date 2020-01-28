70dcba13031ea460e2989c598c17b0a0d2faa5e8
Prashant Narendra SHINGATE <prashantns@imcb.a-star.edu.sg>
2020-01-28 17:59:32 +0800

    adds a python script created for assignment

---
+++ capstone_assignment.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Assignment for Capstone
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print (" Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
